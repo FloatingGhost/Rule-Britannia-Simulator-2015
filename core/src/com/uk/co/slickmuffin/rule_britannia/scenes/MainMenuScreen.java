e7fbe34c015d32069e863abd1589939587ec474c
Hannah Ward <Hannah.ward9001@gmail.com>
2015-11-08 12:10:34 +0000

    Moving to LUA

---
+++ core/src/com/uk/co/slickmuffin/rule_britannia/scenes/MainMenuScreen.java
@@ -0,0 +1,7 @@
+package com.uk.co.slickmuffin.rule_britannia.scenes;
+
+/**
+ * Created by hannah on 06/11/15.
+ */
+public class MainMenuScreen {
+}
---
+++ core/src/com/uk/co/slickmuffin/rule_britannia/scenes/SelectEmpireScreen.java
@@ -0,0 +1,7 @@
+package com.uk.co.slickmuffin.rule_britannia.scenes;
+
+/**
+ * Created by hannah on 07/11/15.
+ */
+public class SelectEmpireScreen {
+}
