98d8d59d7890951c8d954c818008c725e351d66e
Hannah Ward <Hannah.ward9001@gmail.com>
2015-11-08 12:11:54 +0000

    Removed old java stuff

--- core/src/com/uk/co/slickmuffin/rule_britannia/scenes/MainMenuScreen.java
+++
@@ -1,7 +0,0 @@
-package com.uk.co.slickmuffin.rule_britannia.scenes;
-
-/**
- * Created by hannah on 06/11/15.
- */
-public class MainMenuScreen {
-}
--- core/src/com/uk/co/slickmuffin/rule_britannia/scenes/SelectEmpireScreen.java
+++
@@ -1,7 +0,0 @@
-package com.uk.co.slickmuffin.rule_britannia.scenes;
-
-/**
- * Created by hannah on 07/11/15.
- */
-public class SelectEmpireScreen {
-}
